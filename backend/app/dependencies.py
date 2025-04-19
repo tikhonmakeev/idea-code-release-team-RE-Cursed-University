cbb18ef2ee9b9db32d34ef93eec2ea7914b27008
ScoChillOFF <cheatkiller12@gmail.com>
2025-04-19 19:12:57 +0300

    Added message service dependency

--- backend/app/dependencies.py
+++ backend/app/dependencies.py
@@ -3,8 +3,10 @@ from typing import Annotated
 from fastapi import Depends, HTTPException
 from fastapi.security import HTTPBasicCredentials, HTTPBasic
 
+from app.repositories.message_repository import MessageRepository
 from app.repositories.user_repository import UserRepository
 from app.schemas.user import UserAuth, User
+from app.services.message_service import MessageService
 from app.services.user_service import UserService
 from app.services.exc import AuthException
 
@@ -15,8 +17,12 @@ def get_user_service() -> UserService:
     return UserService(UserRepository(SessionLocal))
 
 
+def get_message_service() -> MessageService:
+    return MessageService(MessageRepository(SessionLocal))
+
+
 def get_user_auth(credentials: Annotated[HTTPBasicCredentials, Depends(HTTPBasic())]) -> UserAuth:
-    return UserAuth(username=credentials.username, password=credentials.password)
+    return UserAuth(login=credentials.username, password=credentials.password)
 
 
 async def get_user(user_auth: Annotated[UserAuth, Depends(get_user_auth)],
