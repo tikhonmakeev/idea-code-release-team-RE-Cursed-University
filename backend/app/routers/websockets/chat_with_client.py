c98d92e89159d3de1cb27000ab09f283ca01d975
t.makeev <tihon.makeev@yandex.ru>
2025-04-19 16:57:21 +0300

    ready echo websocket chat

--- backend/app/routers/websockets/chat_with_client.py
+++ backend/app/routers/websockets/chat_with_client.py
@@ -5,7 +5,7 @@ from app.core.logger import get_websocket_logger
 router = APIRouter(prefix="/ws")
 ws_logger = get_websocket_logger()
 
-@router.websocket("/ws/{client_id}")
+@router.websocket("/ws")
 async def websocket_endpoint(websocket: WebSocket, client_id: int, manager: Depends(get_connection_manager_singleton)):
     await manager.connect(websocket)
     ws_logger.info(f"Client #{client_id} connected", extra={"websocket": websocket})
@@ -15,7 +15,8 @@ async def websocket_endpoint(websocket: WebSocket, client_id: int, manager: Depe
             data = await websocket.receive_text()
             # В логи первые 100 символов
             ws_logger.info(f"Received message from client #{client_id}: {data[:100]}", extra={"websocket": websocket})
-
+            # TODO: эхо - удалить перед продом
+            await websocket.send_text(data)
             # TODO: бизнес - логика
             await manager.send_personal_message(f"You wrote: {data}", websocket)
     except WebSocketDisconnect as e:
