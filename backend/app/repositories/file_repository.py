bb4bddab0110ebebec970a7a3a47375deab3690d
t.makeev <tihon.makeev@yandex.ru>
2025-04-19 17:01:52 +0300

    ready file workers without DB

--- backend/app/repositories/file_repository.py
+++ backend/app/repositories/file_repository.py
@@ -1,9 +1,19 @@
 import os
+from fastapi import UploadFile
 
 
-def create_file(dir, name, file):
-    path = dir + "/" + name
-    os.mkdir(dir)
-    with open(path, "wb") as f:
+async def create_file(base_dir: str, user_id: str, filename: str, file: UploadFile.file):
+    folder_path = os.path.join(base_dir, user_id)
+    if os.path.exists(folder_path):
+        os.mkdir(folder_path)
+
+    file_path = os.path.join(folder_path, filename)
+    with open(file_path, "wb") as f:
         f.write(file)
-    return path
+
+    return file_path
+
+
+async def check_file_exists_by_user(base_dir: str, user_id: str, filename: str):
+    folder_path = os.path.join(base_dir, user_id)
+    return os.path.exists(folder_path)
