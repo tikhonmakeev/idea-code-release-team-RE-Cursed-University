2ad86b4f1fa6a877e45ad3dcdba969a63752979b
ScoChillOFF <cheatkiller12@gmail.com>
2025-04-19 19:39:25 +0300

    CORS

--- backend/main.py
+++ backend/main.py
@@ -2,6 +2,8 @@ from fastapi import FastAPI
 from app.core.logger import setup_logging
 import logging
 
+from fastapi.middleware.cors import CORSMiddleware
+
 import uvicorn
 
 from app.routers.rest.auth_handler import router as auth_router
@@ -21,6 +23,13 @@ routers = [
 for router in routers:
     app.include_router(router)
 
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 if __name__ == "__main__":
     uvicorn.run(app)
